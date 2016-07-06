e700c20788347e3d80f25b253289a95d6f8303c7
Michael Schwarz <mschwarz@soft-evolution.com>
2016-07-06 11:38:37 +0200

    * Updated example
    Signed-off-by: Michael Schwarz <das.xwcg@gmail.com>

--- example.js
+++ example.js
@@ -1,13 +1,11 @@
 ﻿/// <reference path="exHelp.js" />
 /// <reference path="exHelp_Date.js" />
-/// <reference path="jquery-2.0.0-vsdoc.js" />
+/// <reference path="exHelp_Element.js" />
 
 (function MyAppSetup(window)
 {
     if (window['exHelp'] == void 0)
         return setTimeout(MyAppSetup, 100, window);
-
-    var myapp = new exHelp();
 
     var baseExtension =
         {
@@ -17,17 +15,17 @@
                     Version: "1.0.0",
                     Author: "Leroy Jenkins"
                 },
-            
+
             myFunction: function (a, b)
             {
                 return parseInt(a) * parseInt(b);
             }
         };
 
-    myapp.extend(baseExtension);
+    exHelp.extend(baseExtension);
 
     // Expose
-    window.m = myapp;
+    window.m = exHelp;
 })(window);
 
 alert(m.myFunction(2, 4));
@@ -35,9 +33,24 @@ alert(m.myFunction(2, 4));
 var lastDate = localStorage.getItem("lastdate");
 if (lastDate)
 {
+    // exDate lets you consistently handle dates
     var lastDate = new m.exDate(lastDate);
-    alert("You last visited on {0}".format(lastDate.format("Y-m-d H:i:s")));
-    alert("That was {0} days ago!".format(lastDate.count.daysBetween(new m.exDate().get.timestamp())));
+    // the .format string extension lets you use C# style string formatting, exDate even gives you PHP style date formatting
+    var lastVisit = "You last visited on {0}".format(lastDate.format("Y-m-d H:i:s"));
+
+    // exDate gives you amazing helper functions to work with dates
+    var span = "That was {0} days ago!".format(lastDate.count.daysBetween(new m.exDate().get.timestamp()));
+
+    // Work with DOM elements like in jQuery without overhead!
+    var div = m.e("<div>", { "class": "box red", "id": "infoBox" });
+
+    // Some examples
+    var p1 = m.e("<p>", { "html": lastVisit });
+    var p2 = m.e("<p>");
+
+    p1.appendTo(div);
+    // You can even chain them!
+    p2.appendTo(div).setHtml(span);;
 }
 
 var nowDate = new m.exDate().get.timestamp();
